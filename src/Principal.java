e0a094c58d942e570b526e910b8ce5505834e48b
Mariana Seidel <marianaaseidel@gmail.com>
2024-05-20 16:26:30 -0300

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+package br.com.alura.viacep.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        String search = " ";
+        System.out.println("Digite o CEP:");
+        search = read.nextLine();
+    }
+}
